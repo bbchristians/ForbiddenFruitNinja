b30251dc788a6227dabf8816e5bfd5813cebe5c6
bbchristians <bbc7909@rit.edu>
2018-01-27 18:49:33 -0500

    updated physics

--- Assets/script/Juice.cs
+++ Assets/script/Juice.cs
@@ -9,19 +9,21 @@ public class Juice : MonoBehaviour {
 
 	public float maxRange = 1;
 
+	public Material[] materials;
+
 	// Use this for initialization
 	void Start () {
 		for (int i = 0; i < juiciness; i++) {
-			Vector3 pos = new Vector3(Random.Range(-1*maxRange,maxRange),0f,0f);
-
-			float rotate = Input.GetAxis ("Vertical") * -90f;
+			Vector3 pos = new Vector3(Random.Range(-1*maxRange,maxRange),0f,0f) + transform.position;
 
 			GameObject go = Instantiate (juice, pos, Quaternion.identity);
-			go.transform.Rotate (Vector3.right * 90);
-			Debug.Log (go.transform);
-			Debug.Log (go.transform.rotation);
+			go.transform.parent = transform;
 
-			Debug.Log (go.transform.position);
+			Material material = materials [Random.Range (0, 2)];
+			go.GetComponent<Renderer> ().material = material;
+
+			Rigidbody rb = go.GetComponent<Rigidbody> ();
+			rb.AddForce (transform.up * 3);
 		}
 		Destroy (gameObject, 5f);
 	}
--- Assets/script/TidePod.cs
+++ Assets/script/TidePod.cs
@@ -10,6 +10,8 @@ public class TidePod : MonoBehaviour {
 
 	public float startForce = 15f;
 
+	public Material[] materials;
+
 	// Use this for initialization
 	void Start () {
 
@@ -17,6 +19,12 @@ public class TidePod : MonoBehaviour {
 
 		rb = GetComponent<Rigidbody2D> ();
 		rb.AddForce (transform.up * startForce * -1, ForceMode2D.Impulse);
+		materials = new Material [2];
+		materials [0] = transform.GetChild (0).GetChild (2).gameObject.GetComponent<Renderer>().material;
+		materials [1] = transform.GetChild (0).GetChild (3).gameObject.GetComponent<Renderer>().material;
+
+		Debug.Log (transform.GetChild (0).GetChild (2).gameObject);			
+
 		Destroy (gameObject, 5f);
 	}
 	
@@ -27,9 +35,15 @@ public class TidePod : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D col) {
 		if (col.tag == "Blade") {
+			GetComponent<CircleCollider2D> ().enabled = false;
+
+			foreach (Transform child in transform) {
+				Destroy (child.gameObject);
+			}
+				
 			GameObject go = Instantiate (juiceProducer, transform);
 			Debug.Log (go);
-			Destroy (gameObject);
+			go.GetComponent<Juice> ().materials = materials;
 		}
 	}
 
