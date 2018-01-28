37409938e49b7a7ab83f8ffa17616d00f194b125
bbchristians <bbc7909@rit.edu>
2018-01-28 11:23:31 -0500

    Updated start screen

---
+++ Assets/ButtonLoader.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonLoader : MonoBehaviour {
+
+	public List<List<Color>> colors;
+
+	public GameObject button;
+
+	public float lasty = 0;
+	public float inc = 50;
+	private float x;
+
+	// Use this for initialization
+	void Start () {
+		colors = new List<List<Color>> ();
+		lasty = transform.position.y;
+		x = transform.position.x;
+		addColors (new Color (24, 32, 133), new Color (253, 91, 16));
+		addColors (new Color (0, 32, 133), new Color (253, 0, 16));
+		//addColors (new Color (), new Color ());
+		// create buttons
+		foreach (List<Color> colors in this.colors) {
+			createButton (colors);
+		}
+	}
+
+	void createButton(List<Color> colors){
+
+		Transform thisTrans = transform;
+		thisTrans.position = new Vector2 (x, lasty);
+		Debug.Log (thisTrans.position);
+		
+		GameObject go = Instantiate(button, thisTrans);
+		(go.GetComponent<ButtonSelector> ()).colors = (colors);
+		lasty -= inc;
+	}	
+
+	void addColors(Color c1, Color c2){
+		List<Color> l = new List<Color> ();
+		l.Add (c1);
+		l.Add (c2);
+		colors.Add (l);
+	}
+}
---
+++ Assets/ButtonSelector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSelector : MonoBehaviour {
+
+	public List<Color> colors;
+	public GameObject go1;
+	public GameObject go2;
+	public GameObject go3;
+	public GameObject go4;
+
+	// Use this for initialization
+	void Start () {
+		go1.GetComponent<Image> ().material.color = colors [0];
+		go2.GetComponent<Image> ().material.color = colors [1];
+		go3.GetComponent<Image> ().material.color = colors [0];
+		go4.GetComponent<Image> ().material.color = colors [1];
+	}
+
+
+}
